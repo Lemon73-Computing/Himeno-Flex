30936ebee4c8fd09aa576bab9109adef74674d3b
Lemon73 <108001913+Lemon73-Computing@users.noreply.github.com>
2023-07-12 02:29:32 +0900

    base system

--- Himeno_Flex/MainPage.xaml.cs
+++ Himeno_Flex/MainPage.xaml.cs
@@ -6,4 +6,37 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		//ベンチマーク前にデータを削除(初期化)
+		mflops1.Text = "";
+		mflops2.Text = "";
+		time.Text = "";
+        loop.Text = "";
+		gosa.Text = "";
+        cpu.Text = "";
+        pentium.Text = "";
+
+        //ベンチマーク開始
+
+        /*
+        [DllImport("himenoBMTxps.dll")]
+        static extern int main();
+        static extern float jacobi(int nn);
+        static extern double fflop(int mx, int my, int mz);
+        static extern double mflops(int nn, double cpu, double flop);
+        static double second();
+        */
+
+        //終了
+        mflops1.Text = "test";
+        mflops2.Text = "test";
+        time.Text = "test";
+        loop.Text = "test";
+        gosa.Text = "test";
+        cpu.Text = "test";
+        pentium.Text = "test";
+
+    }
 }
