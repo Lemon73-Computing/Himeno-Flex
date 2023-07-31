d5fe96a48a2ee58cce8d033f95ef6e23eb3ed4bd
Lemon73 <108001913+Lemon73-Computing@users.noreply.github.com>
2023-08-01 02:54:30 +0900

    .cpp側に合わせて変更

--- HimenoBMTxps/header.h
+++ HimenoBMTxps/header.h
@@ -2,7 +2,7 @@
 
 extern "C"
 {
-    __declspec(dllexport) int main();
+    __declspec(dllexport) int main(double* out1, double* out2, int* out3, float* out4, double* out5, double* out6, double* out7);
     __declspec(dllexport) void initmt();
     __declspec(dllexport) float jacobi(int nn);
     __declspec(dllexport) double fflop(int mx, int my, int mz);
